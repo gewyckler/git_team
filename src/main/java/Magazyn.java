32aae4f97d91882f2060398e1197e384ad89b11c
gewyckler <gewyckler@gmail.com>
2019-06-12 15:09:34 +0200

    Poprawone zwiekszanie liczby w magazynie

--- src/main/java/Magazyn.java
+++ src/main/java/Magazyn.java
@@ -15,11 +15,11 @@ public class Magazyn {
     private Map<String, Integer> listaProduktowWMagazynie = new HashMap<>();
     private Map<String, List<Zamowienie>> mapaZamowien = new HashMap<>();
 
-    public int zwiekszLiczbeWMagazynie(Produkt produkt) {
+    public void zwiekszLiczbeWMagazynie(Produkt produkt) {
 
         for (String liczbaWMagazynie:listaProduktowWMagazynie.keySet()) {
 
-            if (listaProduktowWMagazynie.containsKey(produkt)) { //jes zawiera to powinno zwiekszyc liczbe w magazynie.
+            if (listaProduktowWMagazynie.containsKey(produkt.getNazwa())) { //jes zawiera to powinno zwiekszyc liczbe w magazynie.
                 listaProduktowWMagazynie.values().add(produkt.getIlosc());
             } else {
                 listaProduktowWMagazynie.put(produkt.getNazwa(),produkt.getIlosc()); //jesli nie zawiera to powinno dodac produkt do magazynu i wpisac ile jest tego produktu
@@ -27,5 +27,6 @@ public class Magazyn {
             }
 
         }
+
     }
 }
